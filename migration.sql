cbb7b64b2aa95e31f89b746eb4d531d7aaff5eb0
NutanKushwaha <yourmahto@gmail.com>
2016-05-23 22:28:06 +0530

    paypal

--- migration.sql
+++ migration.sql
@@ -1,1 +1,10 @@
-ALTER TABLE `bg_company` ADD `enable` TINYINT(1) NOT NULL DEFAULT '0' AFTER `cat_suffix`;
+UPDATE `synergyd_kilima`.`bg_config` SET `editable` = '1' WHERE `bg_config`.`config_key` = 'PAYPAL_API_USERNAME';
+
+UPDATE `synergyd_kilima`.`bg_config` SET `editable` = '1' WHERE `bg_config`.`config_key` = 'PAYPAL_API_SIGNATURE';
+
+UPDATE `synergyd_kilima`.`bg_config` SET `editable` = '1' WHERE `bg_config`.`config_key` = 'PAYPAL_API_PASSWORD';
+UPDATE `synergyd_kilima`.`bg_config` SET `config_label` = 'Paypal Email' WHERE `bg_config`.`config_key` = 'PAYPAL_API_USERNAME';
+
+UPDATE `synergyd_kilima`.`bg_config` SET `config_label` = 'Paypal API Signature' WHERE `bg_config`.`config_key` = 'PAYPAL_API_SIGNATURE';
+UPDATE `synergyd_kilima`.`bg_config` SET `config_label` = 'Paypal API Password' WHERE `bg_config`.`config_key` = 'PAYPAL_API_PASSWORD';
+UPDATE `synergyd_kilima`.`bg_config` SET `editable` = '0' WHERE `bg_config`.`config_key` = 'PAYPAL_MERCHENT_EMAIL';
